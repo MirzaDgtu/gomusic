56e88b3991749225a22432e1f7f69dab43025c13
MirzaDgtu <mirzyali@mail.ru>
2024-10-04 02:52:03 +0700

    fc

---
+++ src/modalwindows.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+
+
+export function BuyModalWindow(props){
+    render() 
+        return (
+            <Modal id="buy" tabIndex="-1" role="dialog" isOpen={props.showModal} toggle={props.toggle}>
+                <div role="document"> 
+                    <ModalHeader toggle={props.toggle} className="bg-success text-white">
+                        Buy Item
+                    </ModalHeader>
+                    <ModalBody>
+                    {/*Credit card form*/}
+                    </ModalBody>
+                </div>
+            </Modal>
+        );    
+   }
