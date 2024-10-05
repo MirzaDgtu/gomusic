438f1ab1445482ec0f15681e6d6146339d7abb89
MirzaDgtu <mirzyali@mail.ru>
2024-10-05 13:16:28 +0700

    sc

--- src/modalwindows.js
+++ src/modalwindows.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
+import CreditCardInformation from './CreditCards';
 
 
 export function BuyModalWindow(props){
@@ -11,9 +12,25 @@ export function BuyModalWindow(props){
                         Buy Item
                     </ModalHeader>
                     <ModalBody>
-                    {/*Credit card form*/}
+                    <CreditCardInformation show={true} operation="Charge" toggle={props.toggle} />
                     </ModalBody>
                 </div>
             </Modal>
         );    
    }
+
+   class SignInForm extends React.Component{
+    constructor(){
+        super(props);
+        //this method will get called whenever a user input data into our
+       form
+        this.handleChange = this.handleChange.bind(this);
+        //this method will get called whenever the HTML form gets submitted
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+         errormessage = ''
+        };
+        }
+       
+   }
+   
